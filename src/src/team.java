aaf8ead6c77b07739e334ca11c44e3fd4bc809d2
Anna <ania.buraczek@gmail.com>
2016-04-26 20:43:06 +0200

    new code

--- src/src/team.java
+++ src/src/team.java
@@ -1,3 +1,13 @@
+import java.util.*;
 
 public class team {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<3;i++) {
+            int a=sc.nextInt();
+            System.out.println(a);
+        }
+    }
 }
+
